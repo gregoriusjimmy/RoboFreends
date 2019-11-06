95552cba4e6a8d3b5c77f51984a665f6077b6b68
gregoriusjimmy <gregoriusjimmy@gmail.com>
2019-11-06 20:06:13 +0700

    Updates

---
+++ precache-manifest.74249f498c3d6533b52b50b35a3e8650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5972c317f33ee21196bcc97dbda893",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2d6bc2b635a959a6170c",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d83515dc4ab2dcae1202",
+    "url": "/robofriends/static/css/main.0d63715f.chunk.css"
+  },
+  {
+    "revision": "2d6bc2b635a959a6170c",
+    "url": "/robofriends/static/js/2.a1f41654.chunk.js"
+  },
+  {
+    "revision": "d83515dc4ab2dcae1202",
+    "url": "/robofriends/static/js/main.a6b55732.chunk.js"
+  },
+  {
+    "revision": "8434fe0415e6fa06901e",
+    "url": "/robofriends/static/js/runtime-main.c24a3def.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
